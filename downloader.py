f39c4e0dbceb0382546e9a064ff6efc39cfa8276
Adrian Arencibia <adrian011494@gmail.com>
2020-07-29 10:34:49 -0400

    [FIX] incremental file name in packs

--- downloader.py
+++ downloader.py
@@ -9,7 +9,8 @@ class Downloader:
 
     def download(self, sticker_name):
         sticker_set = self.bot.getStickerSet(sticker_name)
-        sticker_json = {'name': sticker_set.name, 'title': sticker_set.title, 'stickers': [], 'animated':sticker_set.is_animated}
+        sticker_json = {'name': sticker_set.name, 'title': sticker_set.title, 'stickers': [],
+                        'animated': sticker_set.is_animated}
 
         if not os.path.exists(sticker_set.name):
             os.makedirs(sticker_set.name + "/thumb")
@@ -18,13 +19,16 @@ class Downloader:
         if sticker_set.thumb is not None:
             have_thumb = True
             file = self.bot.getFile(sticker_set.thumb.file_id)
-            file.download(sticker_set.name + "/thumb/" + basename(file.file_path))
-            sticker_json['thumb'] = sticker_set.name + "/thumb/" + basename(file.file_path)
-        for st in sticker_set.stickers:
+            filename, extension = splitext(basename(file.file_path))
+            file.download(sticker_set.name + "/thumb/thumb" + extension)
+            sticker_json['thumb'] = sticker_set.name + "/thumb/thumb" + extension
+        for index, st in enumerate(sticker_set.stickers, start=1):
             file = self.bot.getFile(st.file_id)
+            filename, extension = splitext(basename(file.file_path))
+            filename = str(index) + extension
             if not have_thumb:
-                sticker_json['thumb'] = sticker_set.name + "/" + basename(file.file_path)
-            file.download(sticker_set.name + "/" + basename(file.file_path))
-            sticker_json['stickers'].append(sticker_set.name + "/" + basename(file.file_path))
+                sticker_json['thumb'] = sticker_set.name + "/" + filename
+            file.download(sticker_set.name + "/" + filename)
+            sticker_json['stickers'].append(sticker_set.name + "/" + filename)
 
         return sticker_json
