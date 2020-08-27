4b5cb873bc1791c1d2b75b92a38086814d2085fe
Adrian Arencibia <adrian011494@gmail.com>
2020-08-27 12:15:00 -0400

    [FIX] error handler

--- pg_client.py
+++ pg_client.py
@@ -7,14 +7,15 @@ class PgClient:
         self.pg_cl = psycopg2.connect(host=host, port=port, database=db, user=user, password=passwd)
 
     def insert_sticker_pack(self, name, title, thumb, animated):
-        query = "insert into sticker_pack(pack_name,title,thumb,animated,recommended,created_at) values(%s,%s,%s,%s,0, get_unix_timestamp()) ON CONFLICT (pack_name) DO UPDATE set created_at = get_unix_timestamp()"
+        query = "insert into sticker_pack(pack_name,title,thumb,animated,recommended,created_at) values('{0}','{1}','{2}',{3},0, get_unix_timestamp() ) ON CONFLICT (pack_name) DO UPDATE set thumb = '{2}', created_at = get_unix_timestamp() "\
+            .format(name, title, thumb, str(animated))
         try:
             cur = self.pg_cl.cursor()
-            cur.execute(query, (name, title, thumb, str(animated)))
+            cur.execute(query)
             self.pg_cl.commit()
             cur.close()
-        except psycopg2.errors as e:
-            logging.error(e)
+        except psycopg2.Error as e:
+            logging.error(e.pgerror)
             return False
         return True
 
@@ -25,7 +26,7 @@ class PgClient:
             cur.execute(query, (name,))
             self.pg_cl.commit()
             cur.close()
-        except psycopg2.errors as e:
-            logging.error(e)
+        except psycopg2.Error as e:
+            logging.error(e.pgerror)
             return False
         return True
